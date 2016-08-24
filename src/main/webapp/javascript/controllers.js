fc54d2bf7cd90754d53d2cf093b7831344d8978a
Cd <licd2009@outlook.com>
2016-08-24 16:48:21 -0400

    first commit

---
+++ src/main/webapp/javascript/controllers.js
@@ -0,0 +1,36 @@
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function() {
+       cityService.city = $scope.city; 
+    });
+    
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || '2';
+    
+    $scope.appid = '6bbc7a61b85eec393ddf492c9858bffb';
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days, appid:$scope.appid });
+        
+    $scope.convertToFahrenheit = function(degK) {
+        
+        return Math.round((1.8 * (degK - 273)) + 32);
+        
+    }
+    
+    $scope.convertToDate = function(dt) { 
+      
+        return new Date(dt * 1000);
+        
+    };
+    
+}]);
